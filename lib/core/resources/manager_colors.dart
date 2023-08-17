98383a89cb1691ec0f1e22f5bce50ee34e01ac22
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-17 20:05:15 +0300

    Initial commit

---
+++ lib/core/resources/manager_colors.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static const Color primaryColor = Color(0xffBC2C3D);
+  static Color secondaryColor = const Color(0xff25282B);
+  static Color transparent = Colors.transparent;
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+  static Color grayLight = Colors.grey.shade300;
+  static Color white70 = Colors.white70;
+  static Color outBoardingTitleColor = const Color(0xff393939);
+  static const Color progressIndicatorColor = Color(0xff979797);
+}
---
+++ lib/core/resources/manager_strings.dart
@@ -0,0 +1,23 @@
+class ManagerStrings {
+  static const String appName = 'Food Delivery App';
+  static const String outBoardingTitle1 = 'Find A Place Near You';
+  static const String outBoardingTitle2 = 'Choose A Delicious Dish';
+  static const String outBoardingTitle3 = 'Pickup Or Delivery';
+  static const String outBoardingSubTitle1 =
+      'We make it easy to find the food you crave. Enter your address and let us do the rest.';
+  static const String outBoardingSubTitle2 =
+      'Upon request we will connect you with exclusive coupons, special offers and bonuses.';
+  static const String outBoardingSubTitle3 =
+      'We make ordering food fast, simple and free – regardless of whether you order online or cash.';
+  static const String start = 'Start';
+  static const String next = 'Next';
+  static const String skip = 'Skip';
+  static const String welcomeTo = 'Welcome to';
+  static const String signIn = 'Sign in';
+  static const String signUp = 'Sign Up';
+  static const String rememberMe = 'Remember me';
+  static const String forgotYourPassword = 'Forgot your password?';
+  static const String donNotHaveAnAccount = 'Don\'t have an account?';
+  static const String alreadyHaveAccount  = 'Already have account?';
+  static const String or = 'Or';
+}

d53565f26e716065f9de351400fa4f72a33f0a50
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-18 05:43:25 +0300

    add route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/core/features/auth/presentation/view/authentication_view.dart';
+
+import 'core/features/out_boarding/presentation/view/out_boarding_screen.dart';
+import 'core/features/splash/presentation/view/splash_screen.dart';
+import 'core/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +16,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          useMaterial3: true
+      ),
       debugShowCheckedModeBanner: false,
+      initialRoute: Routes.splashScreen,
+      routes: {
+        Routes.splashScreen: (context) => const SplashScreen(),
+        Routes.outBoardingScreen: (context) => const OutBoardingScreen(),
+        Routes.authenticationView: (context) => const AuthenticationView(),
+      },
     );
   }
 }

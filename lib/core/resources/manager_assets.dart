fcee8d80e215687e7dbc663c47ed5efb48b024b6
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-21 01:47:24 +0300

    create Image

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -5,4 +5,8 @@ class ManagerAssets {
   static const String outBoarding3 = 'assets/images/out_boarding/img3.png';
   static const String auth1 = 'assets/images/auth/auth1.png';
   static const String logo1 = 'assets/images/auth/logo1.png';
+  static const String facebook = 'assets/images/auth/facebook.png';
+  static const String google = 'assets/images/auth/google.png';
+  static const String twitter = 'assets/images/auth/twitter.png';
+  static const String unDefineRoute = 'assets/images/un_define_route/un1.png';
 }

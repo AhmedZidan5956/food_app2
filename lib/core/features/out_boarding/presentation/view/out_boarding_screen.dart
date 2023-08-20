92505568e49f79c55bba8ce2efbf8db666b4367e
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-21 01:53:12 +0300

    create Navigator

--- lib/core/features/out_boarding/presentation/view/out_boarding_screen.dart
+++ lib/core/features/out_boarding/presentation/view/out_boarding_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../../constants.dart';
+import '../../../../widgets/constants.dart';
 import '../../../../resources/manager_assets.dart';
 import '../../../../resources/manager_colors.dart';
 import '../../../../resources/manager_font_sizes.dart';
@@ -7,7 +7,7 @@ import '../../../../resources/manager_font_weight.dart';
 import '../../../../resources/manager_height.dart';
 import '../../../../resources/manager_strings.dart';
 import '../../../../resources/manager_width.dart';
-import '../../../../routes.dart';
+import '../../../../route/routes.dart';
 import '../../../../widgets/base_button.dart';
 import '../widget/out_boarding_content.dart';
 import '../widget/progress_indicator.dart';
@@ -38,6 +38,7 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         leading: Visibility(
           visible: isNotFirstPage(),
@@ -54,7 +55,7 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
               Icons.arrow_back_ios_new,
             ),
           ),
-        ),
+        ), 
         backgroundColor: ManagerColors.transparent,
         elevation: Constants.appBarElevation,
         actions: [
@@ -77,7 +78,9 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
                 isVisibleIcon: false,
                 bgColor: ManagerColors.transparent,
                 elevation: Constants.elevationZero,
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.authenticationView);
+                },
               ),
               child: BaseButton(
                 width: ManagerWidth.w10,
@@ -104,105 +107,107 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
         ],
       ),
       body: Container(
-        margin: const EdgeInsets.symmetric(
-          horizontal: ManagerWidth.w30,
-          vertical: ManagerHeight.h34,
-        ),
+        color: ManagerColors.white,
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: PageView(
-                scrollDirection: Axis.horizontal,
-                controller: _pageController,
-                onPageChanged: (int value) {
-                  setState(() {
-                    _currentPageIndex = value;
-                  });
-                },
-                children: const [
-                  OutBoardingContent(
-                    image: ManagerAssets.outBoarding1,
-                    title: ManagerStrings.outBoardingTitle1,
-                    subTitle: ManagerStrings.outBoardingSubTitle1,
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w30,
+            vertical: ManagerHeight.h34,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: PageView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _pageController,
+                  onPageChanged: (int value) {
+                    setState(() {
+                      _currentPageIndex = value;
+                    });
+                  },
+                  children: const [
+                    OutBoardingContent(
+                      image: ManagerAssets.outBoarding1,
+                      title: ManagerStrings.outBoardingTitle1,
+                      subTitle: ManagerStrings.outBoardingSubTitle1,
+                    ),
+                    OutBoardingContent(
+                      image: ManagerAssets.outBoarding2,
+                      title: ManagerStrings.outBoardingTitle2,
+                      subTitle: ManagerStrings.outBoardingSubTitle2,
+                    ),
+                    OutBoardingContent(
+                      image: ManagerAssets.outBoarding3,
+                      title: ManagerStrings.outBoardingTitle3,
+                      subTitle: ManagerStrings.outBoardingSubTitle3,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  progressIndicator(
+                    color: isFirstPage()
+                        ? ManagerColors.primaryColor
+                        : ManagerColors.progressIndicatorColor,
+                    width: isFirstPage() ? ManagerWidth.w20 : ManagerWidth.w8,
                   ),
-                  OutBoardingContent(
-                    image: ManagerAssets.outBoarding2,
-                    title: ManagerStrings.outBoardingTitle2,
-                    subTitle: ManagerStrings.outBoardingSubTitle2,
+                  progressIndicator(
+                    color: isSecondPage()
+                        ? ManagerColors.primaryColor
+                        : ManagerColors.progressIndicatorColor,
+                    width: isSecondPage() ? ManagerWidth.w20 : ManagerWidth.w8,
                   ),
-                  OutBoardingContent(
-                    image: ManagerAssets.outBoarding3,
-                    title: ManagerStrings.outBoardingTitle3,
-                    subTitle: ManagerStrings.outBoardingSubTitle3,
+                  progressIndicator(
+                    color: isLastPage()
+                        ? ManagerColors.primaryColor
+                        : ManagerColors.progressIndicatorColor,
+                    width: isLastPage() ? ManagerWidth.w20 : ManagerWidth.w8,
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                progressIndicator(
-                  color: isFirstPage()
-                      ? ManagerColors.primaryColor
-                      : ManagerColors.progressIndicatorColor,
-                  width: isFirstPage() ? ManagerWidth.w20 : ManagerWidth.w8,
+              const SizedBox(height: ManagerHeight.h40),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: ManagerWidth.w40,
                 ),
-                progressIndicator(
-                  color: isSecondPage()
-                      ? ManagerColors.primaryColor
-                      : ManagerColors.progressIndicatorColor,
-                  width: isSecondPage() ? ManagerWidth.w20 : ManagerWidth.w8,
-                ),
-                progressIndicator(
-                  color: isLastPage()
-                      ? ManagerColors.primaryColor
-                      : ManagerColors.progressIndicatorColor,
-                  width: isLastPage() ? ManagerWidth.w20 : ManagerWidth.w8,
-                ),
-              ],
-            ),
-            const SizedBox(height: ManagerHeight.h40),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: ManagerWidth.w40,
-              ),
-              child: Visibility(
-                visible: isLastPage(),
-                replacement: BaseButton(
-                  isVisibleIcon: true,
-                  textStyle: TextStyle(
-                    fontSize: ManagerFontSizes.s16,
-                    fontWeight: ManagerFontWeight.regular,
-                    color: ManagerColors.white,
+                child: Visibility(
+                  visible: isLastPage(),
+                  replacement: BaseButton(
+                    isVisibleIcon: true,
+                    textStyle: TextStyle(
+                      fontSize: ManagerFontSizes.s16,
+                      fontWeight: ManagerFontWeight.regular,
+                      color: ManagerColors.white,
+                    ),
+                    onPressed: () {
+                      _pageController.animateToPage(
+                        Constants.outBoardingLastPage,
+                        duration: const Duration(milliseconds: 100),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                      );
+                    },
+                    title: ManagerStrings.skip,
                   ),
-                  onPressed: () {
-                    _pageController.animateToPage(
-                      Constants.outBoardingLastPage,
-                      duration: const Duration(milliseconds: 100),
-                      curve: Curves.fastLinearToSlowEaseIn,
-                    );
-                  },
-                  title: ManagerStrings.skip,
-                ),
-
-                child: BaseButton(
-                  isVisibleIcon: true,
-                  textStyle: TextStyle(
-                    fontSize: ManagerFontSizes.s16,
-                    fontWeight: ManagerFontWeight.regular,
-                    color: ManagerColors.white,
+                  child: BaseButton(
+                    isVisibleIcon: true,
+                    textStyle: TextStyle(
+                      fontSize: ManagerFontSizes.s16,
+                      fontWeight: ManagerFontWeight.regular,
+                      color: ManagerColors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, Routes.authenticationView);
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(
-                        context, Routes.authenticationView);
-                  },
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

f61b49a61c5b6de72f4f7019fd5045d5c7f438f8
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-23 02:50:24 +0300

    Add colors

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class ManagerColors {
   static const Color primaryColor = Color(0xffBC2C3D);
   static Color secondaryColor = const Color(0xff25282B);
+  static Color primaryTextColor = const Color(0xff24272a);
   static Color transparent = Colors.transparent;
   static Color white = Colors.white;
   static Color black = Colors.black;

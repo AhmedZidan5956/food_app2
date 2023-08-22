517542afc162e77321c3e90a6cb57adc7b323953
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-22 20:33:19 +0300

    add to EasyLocalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/config/locale/locale_settings.dart';
+import 'package:food_app/core/resources/manager_assets.dart';
 import 'package:food_app/core/resources/manager_colors.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'config/dependancy_injection.dart';
 import 'core/route/routes.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(const MyApp());
+main() async {
+  await initModule();
+  runApp(
+    EasyLocalization(
+      supportedLocales: localeSettings.locales,
+      path: ManagerPaths.translationsPath,
+      startLocale: localeSettings.defaultLocale,
+      fallbackLocale: localeSettings.defaultLocale,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,8 +27,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: ThemeData(
-          useMaterial3: true,
+        useMaterial3: true,
         primaryColor: ManagerColors.white,
       ),
       debugShowCheckedModeBanner: false,

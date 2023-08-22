c5146a252d4b8458618541a3e5a0b03381d9ab0d
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-23 02:50:14 +0300

    Add font Weight

--- lib/core/resources/manager_font_weight.dart
+++ lib/core/resources/manager_font_weight.dart
@@ -2,9 +2,9 @@ import 'dart:ui';
 
 class ManagerFontWeight {
   static const FontWeight w600 = FontWeight.w600;
-  static const FontWeight w500 = FontWeight.w500;
-  static const FontWeight w400 = FontWeight.w400;
   static const FontWeight w200 = FontWeight.w200;
   static const FontWeight bold = FontWeight.w700;
-  static const FontWeight regular = FontWeight.w500;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight italic = FontWeight.w300;
 }

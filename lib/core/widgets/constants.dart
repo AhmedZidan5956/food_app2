453280163fbe0efdc53c3407df0b5af175601661
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-30 18:02:10 +0300

    add ApiRequest & ApiConstants

--- lib/core/widgets/constants.dart
+++ lib/core/widgets/constants.dart
@@ -1,9 +1,11 @@
+import 'package:food_app/core/features/auth/data/response/login_response.dart';
+
 class Constants {
   static const double appBarElevation = 0;
   static const double elevationZero = 0;
   static const double baseButtonElevation = 2;
   static const int splashTime = 3;
-  static const int baseButtonFirstSpacerFlex = 4;
+  static const int baseButtonFirstSpacerFlex = 5;
   static const int baseButtonSecondSpacerFlex = 4;
   static const int pageViewSliderDuration = 300;
   static const bool baseButtonVisibleIcon = false;
@@ -19,4 +21,35 @@ class KeyConstants {
   static const String english = 'en';
   static const String arabicName = 'العربية';
   static const String englishName = 'English';
+  static const String token = 'token';
+  static const String loggedIn = 'logged_in';
+  static const String user = 'user_';
+  static const String userId = '${user}id';
+  static const String userType = '${user}type';
+  static const String userName = '${user}name';
+  static const String userEmail = '${user}email';
+  static const String userAvatar = '${user}avatar';
+  static const String userAvatarOriginal = '${user}avatar_original';
+  static const String userPhone = '${user}phone';
+}
+
+class ApiRequest{
+  static const String baseUrl = 'http://e-commerce-selkroad.actit.ps';
+  static const String api = '$baseUrl/api/v2';
+  static const String apiAuth = '$api/auth';
+  static const String login = '$apiAuth/login';
+  static const String register = '$apiAuth/signup';
+}
+
+class ApiConstants {
+  static const String email = 'email';
+  static const String password = 'password';
+  static const String confirmPassword = 'password_confirmation';
+  static const String authName = 'name';
+  static const String phone = 'phone';
+  static const String registerBy = 'email';
+  static const String acceptLanguage = 'acceptLanguage';
+  static const String errorMessage = 'message';
+  static const String token = 'token';
+
 }

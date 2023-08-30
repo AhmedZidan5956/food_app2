2236a1bb9418949cfba658bd8033b2ce13f268a4
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-30 17:52:55 +0300

    edit StatefulWidget to StatelessWidget

--- lib/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart
+++ lib/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart
@@ -1,3 +1,4 @@
+import 'package:food_app/core/features/auth/presentation/model/login_model.dart';
 import 'package:food_app/extension/extensions.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../widgets/constants.dart';
@@ -16,6 +17,10 @@ class AppSettingsSharedPreferences {
     _sharedPreferences = await SharedPreferences.getInstance();
   }
 
+  clear(){
+    _sharedPreferences.clear();
+  }
+
   Future<void> saveViewedOutBoarding() async {
     await _sharedPreferences.setBool(KeyConstants.outBoardingViewedKey, true);
   }
@@ -29,4 +34,49 @@ class AppSettingsSharedPreferences {
 
   String get defaultLocale =>
       _sharedPreferences.getString(KeyConstants.localeKey).parseLocale();
+
+  Future<void> setToken(String token) async {
+    await _sharedPreferences.setString(KeyConstants.token, token);
+  }
+  Future<void> saveUserInfo(User user) async {
+    await _sharedPreferences.setInt(
+      KeyConstants.userId,
+      user.id,
+    );
+    await _sharedPreferences.setString(
+      KeyConstants.userType,
+      user.type,
+    );
+    await _sharedPreferences.setString(
+      KeyConstants.userName,
+      user.name,
+    );
+    await _sharedPreferences.setString(
+      KeyConstants.userEmail,
+      user.email,
+    );
+    await _sharedPreferences.setString(
+      KeyConstants.userAvatar,
+      user.avatar,
+    );
+    await _sharedPreferences.setString(
+      KeyConstants.userAvatarOriginal,
+      user.avatarOriginal,
+    );
+    await _sharedPreferences.setString(
+      KeyConstants.userPhone,
+      user.phone,
+    );
+  }
+
+
+  String get defaultToken =>
+      _sharedPreferences.getString(KeyConstants.token).onNull();
+
+  Future<void> setLoggedIn() async {
+    await _sharedPreferences.setBool(KeyConstants.loggedIn, true);
+  }
+
+  bool get loggedIn =>
+      _sharedPreferences.getBool(KeyConstants.loggedIn).onNull();
 }

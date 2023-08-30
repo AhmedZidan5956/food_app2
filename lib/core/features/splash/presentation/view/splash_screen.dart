5b6e706ac826f62466d31fe17d7d2f1bdf112f00
AhmedZidan5956 <ahmedZidan5956@gmail.com>
2023-08-30 17:53:13 +0300

    edit StatefulWidget to StatelessWidget

--- lib/core/features/splash/presentation/view/splash_screen.dart
+++ lib/core/features/splash/presentation/view/splash_screen.dart
@@ -1,37 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/core/resources/manager_height.dart';
-import 'package:get/get.dart';
-import '../../../../storage/local/database/shared_preferences/app_settings_shared_preferences.dart';
-import '../../../../widgets/constants.dart';
 import '../../../../resources/manager_assets.dart';
 import '../../../../resources/manager_font_sizes.dart';
 import '../../../../resources/manager_strings.dart';
-import '../../../../route/routes.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(
-       const Duration(
-        seconds: Constants.splashTime,
-      ),
-          () => AppSettingsSharedPreferences().outBoardingViewed
-          ? Get.offAllNamed(Routes.authenticationView)
-          : Get.offAllNamed(Routes.outBoardingScreen),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
@@ -45,8 +22,8 @@ class _SplashScreenState extends State<SplashScreen> {
                   Image.asset(
                     ManagerAssets.splash3,
                   ),
-                   SizedBox(height: ManagerHeight.h12),
-                   Text(
+                  SizedBox(height: ManagerHeight.h12),
+                  Text(
                     ManagerStrings.appName,
                     style: const TextStyle(
                       fontSize: ManagerFontSizes.s20,
